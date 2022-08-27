c410d64c0b21b162b7ab3e6087dd069eb6ef5be3
skidoodle <skidoodle>
2022-08-27 07:46:41 +0200

    Prettier.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # S3Count
+
 An Express.js app that counts the amount of objects and the size that have been used in any S3 compatible solution.
 
 ## Installation
@@ -10,12 +11,15 @@ git clone https://github.com/skidoodle/s3count.git
 ```
 
 ## Usage
+
 Install the dependencies with [npm](https://nodejs.org/)
+
 ```
 npm install
 ```
 
 Run the app
+
 ```
 npm start
 ```
@@ -23,4 +27,5 @@ npm start
 You might want to use a process manager like [pm2](https://github.com/Unitech/pm2) and a reverse proxy such as [nginx](https://nginx.org/) to run the app in a production environment.
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,51 +1,54 @@
-require('dotenv').config()
-const { PORT, BUCKET, ACCESSKEY, SECRETKEY, ENDPOINT, REGION } = process.env
-const express = require('express')
-const aws = require('aws-sdk')
-const app = express()
-const port = PORT
+require("dotenv").config();
+const { PORT, BUCKET, ACCESSKEY, SECRETKEY, ENDPOINT, REGION } = process.env;
+const express = require("express");
+const aws = require("aws-sdk");
+const app = express();
+const port = PORT;
 
-app.get('/', async (req, res) => {
-    aws.config.s3 = ({
-        accessKeyId: ACCESSKEY,
-        secretAccessKey: SECRETKEY,
-        region: REGION,
-        endpoint: ENDPOINT,
-        signatureVersion: 'v4'
-    })
-    let isTruncated = true
-    let startAfter = null
-    let objects = 0
-    let size = 0
+app.get("/", async (req, res) => {
+  aws.config.s3 = {
+    accessKeyId: ACCESSKEY,
+    secretAccessKey: SECRETKEY,
+    region: REGION,
+    endpoint: ENDPOINT,
+    signatureVersion: "v4",
+  };
+  let isTruncated = true;
+  let startAfter = null;
+  let objects = 0;
+  let size = 0;
 
-    const s3 = new aws.S3()
+  const s3 = new aws.S3();
 
-    while(isTruncated) {
-        let params = { Bucket: BUCKET }
-        
-        if(startAfter) {
-            params.StartAfter = startAfter
-        }
-        const data = await s3.listObjectsV2(params).promise()
+  while (isTruncated) {
+    let params = { Bucket: BUCKET };
 
-        data.Contents?.forEach((object) => {
-            objects++
-            size += object.Size / 1024 / 1024 / 1024
-        })
-
-        isTruncated = data.IsTruncated
-        if (isTruncated) {
-            startAfter = data.Contents.slice(-1)[0].Key
-        }
+    if (startAfter) {
+      params.StartAfter = startAfter;
     }
-    res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59')
-    res.status(200).json({ object: objects, size: Number(size.toFixed(2)) })
-})
+    const data = await s3.listObjectsV2(params).promise();
 
-app.get('*', async (req, res) => {
-    res.status(404).json(404)
-})
+    data.Contents?.forEach((object) => {
+      objects++;
+      size += object.Size / 1024 / 1024 / 1024;
+    });
+
+    isTruncated = data.IsTruncated;
+    if (isTruncated) {
+      startAfter = data.Contents.slice(-1)[0].Key;
+    }
+  }
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
+  res.status(200).json({ object: objects, size: Number(size.toFixed(2)) });
+});
+
+app.get("*", async (req, res) => {
+  res.status(404).json(404);
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
